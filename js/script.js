9decc13cfe2f5dddaac571fa73a0383de673462e
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-07-28 18:49:09 +0200

    Changed name from 8.3 to 8.4

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // script.js
 
-
+// napisz komentarze… //
+// cmd+shift+D… //
+// cmd+shift+D… //
 
 var name = prompt('Enter your name');
 alert('Cześć, ' + name);
 console.log('Cześć, ' + name);
+
+
+
+/*	długie i krótkie komentarze…	*/
+/*	długie i krótkie komentarze…	*/
+/*	długie i krótkie komentarze…	*/
